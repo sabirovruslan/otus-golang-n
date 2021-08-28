7824eb2c7da7782b80dfbed1d405cb8f12724312
Ruslan <sabirov@worksolutions.ru>
2021-08-28 22:35:08 +0300

    HW4 Save point

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,66 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size     int
+	headItem *ListItem
+	tailItem *ListItem
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	return l.headItem
+}
+
+func (l *list) Back() *ListItem {
+	return l.tailItem
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	i := &ListItem{v, nil, nil}
+
+	if l.Len() == 0 {
+		l.headItem, l.tailItem = i, i
+	} else {
+		i.Next, l.headItem.Prev = l.headItem, i
+		l.headItem = i
+	}
+	l.size++
+
+	return i
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	i := &ListItem{v, nil, nil}
+
+	if l.Len() == 0 {
+		l.headItem, l.tailItem = i, i
+	} else {
+		i.Prev, l.tailItem.Next = l.tailItem, i
+		l.tailItem = i
+	}
+	l.size++
+
+	return i
+}
+
+func (l *list) Remove(i *ListItem) {
+	switch {
+	case i.Next == nil:
+		l.tailItem = i.Prev
+	case i.Prev == nil:
+		l.headItem = i.Next
+	default:
+		i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+	}
+
+	l.size--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+
 }
 
 func NewList() List {
